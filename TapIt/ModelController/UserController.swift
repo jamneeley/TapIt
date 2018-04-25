660020e33764ea927336dbe7a3c2f5421c6b7df6
Jamneeley <jamneeley@gmail.com>
2018-04-25 17:08:51 -0600

    added files

---
+++ TapIt/ModelController/UserController.swift
@@ -0,0 +1,9 @@
+//
+//  UserController.swift
+//  TapIt
+//
+//  Created by James Neeley on 4/25/18.
+//  Copyright © 2018 JamesNeeley. All rights reserved.
+//
+
+import Foundation
---
+++ TapIt/ViewController/ScoreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScoreTableViewCell.swift
+//  TapIt
+//
+//  Created by James Neeley on 4/25/18.
+//  Copyright © 2018 JamesNeeley. All rights reserved.
+//
+
+import UIKit
+
+class ScoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
