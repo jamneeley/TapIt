70b8ea2d07acd6668f9316eca19364026ab5d67e
Jamneeley <jamneeley@gmail.com>
2018-04-25 17:49:59 -0600

    added constraints and image for button

--- TapIt/ViewController/MainViewController.swift
+++ TapIt/ViewController/MainViewController.swift
@@ -9,27 +9,76 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    let highScoresButton = UIButton()
+    let scoreLabel = UILabel()
+    let actionLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.backgroundColor = .gray
+        setupObjects()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupObjects() {
+        setupHighScoreButton()
+        setupScoreLabel()
+        setupActionLabel()
     }
-    */
+    
+    
+    func setupHighScoreButton() {
+        view.addSubview(highScoresButton)
+        highScoresButton.backgroundColor = .yellow
+        setupHighScoreButtonConstraints()
+    }
+    
+    func setupScoreLabel() {
+        view.addSubview(scoreLabel)
+        scoreLabel.backgroundColor = .blue
+        setupScoreLabelConstraints()
+        
+    }
 
+    
+    func setupActionLabel() {
+        view.addSubview(actionLabel)
+        actionLabel.backgroundColor = .red
+        setupActionLabelConstraints()
+    }
+    
+    func setupHighScoreButtonConstraints() {
+        highScoresButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: highScoresButton, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 20).isActive = true
+        
+        NSLayoutConstraint(item: highScoresButton, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: highScoresButton, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant:  view.frame.height / 10).isActive = true
+        
+        NSLayoutConstraint(item: highScoresButton, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: view.frame.width / -8).isActive = true
+        
+    }
+    
+    func setupScoreLabelConstraints() {
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: scoreLabel, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 20).isActive = true
+        
+        NSLayoutConstraint(item: scoreLabel, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: scoreLabel, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: view.frame.height / 10).isActive = true
+        
+        NSLayoutConstraint(item: scoreLabel, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: view.frame.width / 4).isActive = true
+    }
+
+    
+    func setupActionLabelConstraints() {
+        actionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: actionLabel, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: view.frame.height / 2.5).isActive = true
+        
+        NSLayoutConstraint(item: actionLabel, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: view.frame.width / 10).isActive = true
+        
+        NSLayoutConstraint(item: actionLabel, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: view.frame.height / -2.5).isActive = true
+        
+        NSLayoutConstraint(item: actionLabel, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: view.frame.width / -10).isActive = true
+    }
 }
